f8d87c111a2378af81fd60553d82ee369dc00ace
micstach <micstach@poczta.onet.pl>
2017-02-26 12:41:35 +0100

    test repository

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express()
+
+app.get('/add/x/:x/y/:y', function (req, res) {
+    var x = parseInt(req.params.x);
+    var y = parseInt(req.params.y);
+    res.send(`${x+y}`);
+})
+
+app.listen(3000, function () {
+    console.log('Server is running, CTRL+C to stop');
+});
