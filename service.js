f5dd4f5749e9abf2f3295abe63d55853be470962
Michal Stachanczyk <Michal.Stachanczyk@autodesk.com>
2017-03-01 13:14:46 +0100

    show me url

--- service.js
+++ service.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var app = express()
 
+app.get('/', function(req, res){
+  res.send(req.headers.host + req.originalUrl);
+});
+
 app.get('/add/:x/:y', function (req, res) {
     console.log(`Input params: x=${req.params.x}, y=${req.params.y}`);
     var x = parseInt(req.params.x);
